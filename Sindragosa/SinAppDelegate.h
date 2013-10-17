c3411932d165646cbf6568f41ba42e9f2379fea3
tonic <tonicbupt@gmail.com>
2013-10-17 14:11:49 +0800

    Initial Commit

---
+++ Sindragosa/SinAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  SinAppDelegate.h
+//  Sindragosa
+//
+//  Created by tonic on 13-10-17.
+//  Copyright (c) 2013年 tonic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SinAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Sindragosa/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Sindragosa
+//
+//  Created by tonic on 13-10-17.
+//  Copyright (c) 2013年 tonic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
